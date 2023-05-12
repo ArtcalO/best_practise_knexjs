b94d09f2472a315e7fe4d1f2dbaf249bd05d8827
ArtcalO <niyca14@gmail.com>
2023-05-12 13:35:27 +0200

    add new models

--- db/migrations/20230511062618_init.js
+++ db/migrations/20230511062618_init.js
@@ -1,15 +1,35 @@
 exports.up = function(knex) {
-		return knex.schema
-				.createTable('person', table=>{
-						table.increments('id');
-						table.string('email').notNullable().unique();
-						table.string("first_name").notNullable();
-						table.string("last_name").notNullable();
-						table.timestamps(true, true);
+	return knex.schema
+		.createTable('invoice', table=>{
+			table.increments('id');
+			table.integer('number').notNullable();
+			table.string("customer_name").notNullable();
+			table.float("total").notNullable();
+			table.timestamps(true, true);
 
-				})
+		})
+		.createTable('items', table=>{
+			table.increments('id');
+			table.string("name").notNullable();
+			table.float("sale_price").notNullable();
+			table.timestamps(true, true);
+
+		})
+		.createTable('invoice_items', table=>{
+			table.increments('id');
+			table.integer('invoice_id').reference('id').inTable('invoice');
+			table.integer('item_id').reference('id').inTable('items');
+			table.integer("quantity").notNullable();
+			table.float("unit_price").notNullable();
+			table.float("total_price").notNullable();
+			table.timestamps(true, true);
+
+		})
 };
 
 exports.down = function(knex) {
-		return knex.schema.dropTable('person')
+		return knex.schema
+			.dropTableIfExists('invoice')
+			.dropTableIfExists('items')
+			.dropTableIfExists('invoice_items')
 };
