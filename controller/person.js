66168899ae3ece63001f5a327a29da88990de6ef
ArtcalO <niyca14@gmail.com>
2023-05-12 12:47:37 +0200

    added in browser

--- controller/person.js
+++ controller/person.js
@@ -3,8 +3,8 @@ class PersonController{
 	async createPerson(req, res){
 		try{
 			console.log("============", req.body)
-			const id =  await personService.createPerson(req.body)
-			res.status(201).json(id)
+			const id =  await personService.createPerson(req.body);
+			res.status(201).json(id);
 		} catch (err){
 			console.log(err)
 			res.status(500).json("Une erreur s'est produite");
