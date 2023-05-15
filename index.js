cb7eedceb49c31ae51e96f94f1920c7adc5d890e
ShwaibLeBon <ndabashinzes@gmail.com>
2023-05-15 06:23:25 +0200

    added package for managing cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+var cors = require('cors')
 const router = require('./router');
 const setupDB = require('./db/db-setup')
 setupDB();
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.get("/", (req, res) => res.status(200).json({ message: "welcome" }));
 app.use(router);
